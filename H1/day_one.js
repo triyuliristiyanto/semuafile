d339e66095e7c7c62f3bf1e5ab5863e829cf991b
asdan <triyuliristiyanto@gmail.com>
2020-02-13 14:57:58 +0700

    added prev folders

---
+++ H1/day_one.js
@@ -0,0 +1,43 @@
+// VARIABLE  & TYPE DATA & COMMENT
+
+// VARIABLE = WADAH YANG PUNYA NAMA
+
+// //DECLARATION
+// var kelas ;
+// // assignment
+// kelas = 'Purwadika'
+
+// //declare + assignment
+// var kelas = ' Purwadika'
+
+// kelas = 'Web Mobile'
+// var kelas = 'Data Science'
+
+
+// console.log(kelas)
+
+// integer, string, bolean (TIPE DATA)
+
+// var nama = 'fikri123' // string
+// var num  = 123456 //integer
+// var bol = true //boolean
+// var siapa = nama
+
+// console.log (kelas , siapa)
+
+// var nama = 'fikri'
+// var nama = 'siapa'
+
+// console.log(nama)
+
+// let kelas = 'purwadika'
+// kelas = 'web mobile'
+
+// console.log (kelas)
+
+let number = 5 
+if(number > 10){
+    console. log('Besar')
+} else{
+    console.log('Kecil')
+}
